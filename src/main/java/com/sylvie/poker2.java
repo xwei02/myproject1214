29ef157cd6e5896bfaa85f5c550ebbc168a452d4
USER <sylvie.wu@hotmail.com>
2021-12-14 14:05:38 +0800

    commit

--- src/main/java/com/sylvie/poker2.java
+++ src/main/java/com/sylvie/poker2.java
@@ -7,9 +7,12 @@ public class poker2 {
         int n = 0x00A9;
         System.out.println((char)n);
         char[] types = {'c', 'd', 'h', 's'};
+        String tt = "cdhs";
+        System.out.println(tt.length());
+        System.out.println(tt.charAt(0));
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 13; j++) {
-                System.out.print((j + 1) + "" + types[i]);
+                System.out.print((j + 1) + "" + tt.charAt(0));
                 System.out.print(" ");
             }
             System.out.println();
